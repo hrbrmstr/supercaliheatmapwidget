0c43c59566497b2f7188b5e67a8dfbbbf9dc7745
hrbrmstr <bob@rud.is>
2019-02-01 19:05:08 -0500

    R package repo initialization complete

---
+++ R/supercaliheatmapwidget-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/supercaliheatmapwidget>
+#' - BugReports: <https://gitlab.com/hrbrmstr/supercaliheatmapwidget/issues>
+#' 
+#' @md
+#' @name supercaliheatmapwidget
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
